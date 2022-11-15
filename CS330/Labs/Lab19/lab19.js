dc38efea2bd9732e2d98818f87d4a2655e7321c0
evanSturtevant <81034560+evanSturtevant@users.noreply.github.com>
2022-11-15 17:48:26 -0500

    lab19 bug

--- CS330/Labs/Lab19/lab19.js
+++ CS330/Labs/Lab19/lab19.js
@@ -58,12 +58,12 @@ function configureTexture( image ) {
     texture = gl.createTexture();
     gl.bindTexture(gl.TEXTURE_2D, texture);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB,
-        gl.RGB, gl.UNSIGNED_BYTE, image);
+         gl.RGB, gl.UNSIGNED_BYTE, image);
     gl.generateMipmap(gl.TEXTURE_2D);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER,
-        gl.NEAREST_MIPMAP_LINEAR);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER,
-        gl.NEAREST);
+                      gl.NEAREST_MIPMAP_LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+
     gl.uniform1i(gl.getUniformLocation(program, "uTextureMap"), 0);
 }
 
