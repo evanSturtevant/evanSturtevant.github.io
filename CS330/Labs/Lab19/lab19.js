916c7c8b9fcfc8774883e3499a65a3980e813a7d
evanSturtevant <81034560+evanSturtevant@users.noreply.github.com>
2022-11-15 17:44:48 -0500

    Lab 19

--- CS330/Labs/Lab19/lab19.js
+++ CS330/Labs/Lab19/lab19.js
@@ -55,7 +55,16 @@ var vertexColors = [
 window.onload = init;
 
 function configureTexture( image ) {
-
+    texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB,
+        gl.RGB, gl.UNSIGNED_BYTE, image);
+    gl.generateMipmap(gl.TEXTURE_2D);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER,
+        gl.NEAREST_MIPMAP_LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER,
+        gl.NEAREST);
+    gl.uniform1i(gl.getUniformLocation(program, "uTextureMap"), 0);
 }
 
 function triangle (a,b,c,triNum)
@@ -98,6 +107,9 @@ function init()
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
     colorTetra();
+
+    var image = document.getElementById("texImage");
+    configureTexture(image);
     
     //
     //  Configure WebGL
